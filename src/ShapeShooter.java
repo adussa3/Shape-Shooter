b4511b0ccccc57697be23bd6eff4b08ed18c1b20
Anunoy Dussa <adussa3@gmail.com>
2021-11-08 18:32:27 -0500

    Added ShapeShooter class

---
+++ src/ShapeShooter.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class ShapeShooter {
+    private static void createAndShowGUI() {
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+}
