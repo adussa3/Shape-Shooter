84941a81ada20e658e8aeb9f770125f3744a279d
Anunoy Dussa <adussa3@gmail.com>
2021-11-09 12:06:37 -0500

    Pushed all changes

--- src/Enemy.java
+++ src/Enemy.java
@@ -1,52 +1,52 @@
-import java.awt.*;
-import java.util.Random;
-
-public class Enemy extends Shape {
-    private static final Random RANDOM = new Random();
-    private static final int MIN_SPEED = 1;
-    private static final int MAX_SPEED = 5;
-
-    private int speed;
-    private Direction direction;
-
-    public Enemy(Color color, int health, int sides) {
-        this(color, health, sides, 1);
-    }
-
-    public Enemy(Color color, int health, int sides, int speed) {
-        super(color, health, sides);
-        this.speed = speed;
-        this.direction = getRandomDirection();
-    }
-
-//    public Enemy(Color color, int health, int sides, int speed, Direction direction) {
+//import java.awt.*;
+//import java.util.Random;
+//
+//public class Enemy {
+//    private static final Random RANDOM = new Random();
+//    private static final int MIN_SPEED = 1;
+//    private static final int MAX_SPEED = 5;
+//
+//    private int speed;
+//    private Direction direction;
+//
+//    public Enemy(Color color, int health, int sides) {
+//        this(color, health, sides, MIN_SPEED);
+//    }
+//
+//    public Enemy(Color color, int health, int sides, int speed) {
 //        super(color, health, sides);
 //        this.speed = speed;
+//        this.direction = getRandomDirection();
+//    }
+//
+////    public Enemy(Color color, int health, int sides, int speed, Direction direction) {
+////        super(color, health, sides);
+////        this.speed = speed;
+////        this.direction = direction;
+////    }
+//
+//    public int getSpeed() {
+//        return speed;
+//    }
+//
+//    public void setSpeed(int speed) {
+//        this.speed = speed;
+//    }
+//
+//    public Direction getDirection() {
+//        return direction;
+//    }
+//
+//    public void setDirection(Direction direction) {
 //        this.direction = direction;
 //    }
-
-    public int getSpeed() {
-        return speed;
-    }
-
-    public void setSpeed(int speed) {
-        this.speed = speed;
-    }
-
-    public Direction getDirection() {
-        return direction;
-    }
-
-    public void setDirection(Direction direction) {
-        this.direction = direction;
-    }
-
-    private int getRandomSpeed() {
-        return RANDOM.nextInt(MAX_SPEED - MIN_SPEED) + MIN_SPEED;
-    }
-
-    private Direction getRandomDirection() {
-        Direction[] directions = Direction.values();
-        return directions[RANDOM.nextInt(directions.length)];
-    }
-}
+//
+//    private int getRandomSpeed() {
+//        return RANDOM.nextInt(MAX_SPEED - MIN_SPEED) + MIN_SPEED;
+//    }
+//
+//    private Direction getRandomDirection() {
+//        Direction[] directions = Direction.values();
+//        return directions[RANDOM.nextInt(directions.length)];
+//    }
+//}
