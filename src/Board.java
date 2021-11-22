d34cbafb2b8aced87a18e11620369c798b947706
Elina <eebby3@gatech.edu>
2021-11-21 23:41:07 -0500

    Implemented listeners and overrode methods

--- src/Board.java
+++ src/Board.java
@@ -9,7 +9,7 @@ import java.util.Random;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 
-public class Board extends JPanel implements ActionListener {
+public class Board extends JPanel implements MouseListener, MouseMotionListener, ActionListener {
 
     private Timer timer;
     private ShapeShooter SpaceShooter;
@@ -21,6 +21,7 @@ public class Board extends JPanel implements ActionListener {
     private boolean isStroke;
     private boolean isDragging;
     private ArrayList<Point2D> strokePoints;
+    private Result lastResult = null;
 
     private final Random random = new Random();
     private final int ICRAFT_X = SpaceShooter.WIDTH / 2 - 50;
@@ -60,60 +61,63 @@ public class Board extends JPanel implements ActionListener {
 //                spaceship.fire();
 //            }
 //        });
-
-        addMouseMotionListener(new MouseMotionListener() {
-            @Override
-            public void mouseDragged(MouseEvent e) {
-                if (e.isPopupTrigger()) {
-                    isStroke = true;
-                    strokePoints.add(e.getPoint());
-                    repaint();
-                } else {
-                    //if it just started a drag
-                    if (!isDragging) {
-                        isDragging = true;
-                    }
-                }
-            }
-
-            @Override
-            public void mouseMoved(MouseEvent e) {
-
-            }
-        });
-
-        addMouseListener(new MouseListener() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-
-            }
-
-            @Override
-            public void mousePressed(MouseEvent e) {
-
-            }
-
-            @Override
-            public void mouseReleased(MouseEvent e) {
-                if (isStroke) {
-                    isStroke = false;
-                    stroke(strokePoints);
-                    strokePoints = new ArrayList<>();
-                    repaint();
-                }
-                isDragging = false;
-            }
-
-            @Override
-            public void mouseEntered(MouseEvent e) {
-
-            }
-
-            @Override
-            public void mouseExited(MouseEvent e) {
-
-            }
-        });
+//
+//        addMouseMotionListener(new MouseMotionListener() {
+//            @Override
+//            public void mouseDragged(MouseEvent e) {
+//                Point p = SwingUtilities.convertPoint(this, e.getPoint(), SpaceShooter.panel);
+//                points.add(p);
+//                repaint();
+//                if (e.isPopupTrigger()) {
+//                    isStroke = true;
+//                    strokePoints.add(e.getPoint());
+//                    repaint();
+//                } else {
+//                    //if it just started a drag
+//                    if (!isDragging) {
+//                        isDragging = true;
+//                    }
+//                }
+//            }
+//
+//            @Override
+//            public void mouseMoved(MouseEvent e) {
+//
+//            }
+//        });
+//
+//        addMouseListener(new MouseListener() {
+//            @Override
+//            public void mouseClicked(MouseEvent e) {
+//
+//            }
+//
+//            @Override
+//            public void mousePressed(MouseEvent e) {
+//
+//            }
+//
+//            @Override
+//            public void mouseReleased(MouseEvent e) {
+//                if (isStroke) {
+//                    isStroke = false;
+//                    stroke(strokePoints);
+//                    strokePoints = new ArrayList<>();
+//                    repaint();
+//                }
+//                isDragging = false;
+//            }
+//
+//            @Override
+//            public void mouseEntered(MouseEvent e) {
+//
+//            }
+//
+//            @Override
+//            public void mouseExited(MouseEvent e) {
+//
+//            }
+//        });
 
         addMouseWheelListener(new MouseWheelListener() {
             @Override
@@ -382,6 +386,56 @@ public class Board extends JPanel implements ActionListener {
         while (aliens.size() < 10) {
             generateAlien();
         }
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        strokePoints = new ArrayList<Point2D>();
+        // translate to canvas coordinate system
+        Point p = SwingUtilities.convertPoint(this, e.getPoint(), SpaceShooter.panel);
+        strokePoints.add(p);
+        lastResult = null;
+        repaint();
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        // translate to canvas coordinate system
+        Point p = SwingUtilities.convertPoint(this, e.getPoint(), SpaceShooter.panel);
+        strokePoints.add(p);
+        Result result = dollarRecognizer.recognize(strokePoints);
+        stroke(strokePoints);
+        System.out.println(result.getName());
+        
+        this.lastResult = result;
+        repaint();
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+        Point p = SwingUtilities.convertPoint(this, e.getPoint(), SpaceShooter.panel);
+        strokePoints.add(p);
+        repaint();
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+
     }
 
 
